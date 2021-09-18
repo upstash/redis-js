bd7b29cf68c3f023bf90f69920cdbe5cd9b07304
Adem ilter <ademilter@gmail.com>
2021-09-18 15:38:21 +0300

    fix

--- test/incrbyfloat.test.ts
+++ test/incrbyfloat.test.ts
@@ -2,11 +2,13 @@ import { set, incrbyfloat } from '../src';
 
 describe('incrbyfloat command', () => {
   const key = 'myKey';
+  const value1 = 2.3;
+  const value2 = 0.4;
 
   it('basic', async () => {
-    await set(key, 2.3);
+    await set(key, value1);
 
-    const { data } = await incrbyfloat(key, 0.1);
-    expect(data).toBe('2.4');
+    const { data } = await incrbyfloat(key, value2);
+    expect(data).toBe((value1 + value2).toString());
   });
 });

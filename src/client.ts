2cffc3e734acfc772411d0f6ca3419e362853d58
Adem ilter <ademilter@gmail.com>
2021-09-18 14:19:02 +0300

    getset

--- src/client.ts
+++ src/client.ts
@@ -60,6 +60,12 @@ export default function client(url?: string, token?: string) {
     return promise;
   }
 
+  /*
+  ------------------------------------------------
+  String
+  ------------------------------------------------
+   */
+
   // APPEND
   function append(
     key: string,
@@ -67,6 +73,121 @@ export default function client(url?: string, token?: string) {
     callback?: Callback
   ): MethodReturn {
     return request(callback, 'append', key, value);
+  }
+
+  // DECR
+  function decr(key: string, callback?: Callback): MethodReturn {
+    return request(callback, 'decr', key);
+  }
+
+  // DECRBY
+  function decrby(
+    key: string,
+    decrement: number,
+    callback?: Callback
+  ): MethodReturn {
+    return request(callback, 'decrby', key, decrement);
+  }
+
+  // GET
+  function get(key: string, callback?: Callback): MethodReturn {
+    return request(callback, 'get', key);
+  }
+
+  // GETRANGE
+  function getrange(
+    key: string,
+    start: number,
+    end: number,
+    callback?: Callback
+  ): MethodReturn {
+    return request(callback, 'getrange', key, start, end);
+  }
+
+  // GETSET
+  function getset(
+    key: string,
+    value: string,
+    callback?: Callback
+  ): MethodReturn {
+    return request(callback, 'getset', key, value);
+  }
+
+  // MSET
+  function mset(values: string[], callback?: Callback): MethodReturn {
+    return request(callback, 'mset', ...values);
+  }
+
+  // SET
+  function set(key: string, value: string, callback?: Callback): MethodReturn {
+    return request(callback, 'set', key, value);
+  }
+
+  /*
+  ------------------------------------------------
+  ...
+  ------------------------------------------------
+ */
+
+  // DEL
+  function del(
+    key: string,
+    fields: string[],
+    callback?: Callback
+  ): MethodReturn {
+    return request(callback, 'del', key, ...fields);
+  }
+
+  // EXISTS
+  function exists(key: string | string[], callback?: Callback): MethodReturn {
+    if (Array.isArray(key)) {
+      return request(callback, 'exists', ...key);
+    }
+    return request(callback, 'exists', key);
+  }
+
+  // EXPIRE
+  function expire(
+    key: string,
+    seconds: number,
+    option?: 'NX' | 'XX' | 'GT' | 'LT',
+    callback?: Callback
+  ): MethodReturn {
+    if (option) {
+      return request(callback, 'expire', key, seconds, option);
+    }
+    return request(callback, 'expire', key, seconds);
+  }
+
+  // FLUSHALL
+  function flushall(mode?: 'ASYNC', callback?: Callback): MethodReturn {
+    if (mode) {
+      return request(callback, 'flushall', mode);
+    }
+    return request(callback, 'flushall');
+  }
+
+  // FLUSHDB
+  function flushdb(mode?: 'ASYNC', callback?: Callback): MethodReturn {
+    if (mode) {
+      return request(callback, 'flushdb', mode);
+    }
+    return request(callback, 'flushdb');
+  }
+
+  // KEYS
+  function keys(pattern: string, callback?: Callback): MethodReturn {
+    return request(callback, 'keys', pattern);
+  }
+
+  // DBSIZE
+  function dbsize(callback?: Callback): MethodReturn {
+    return request(callback, 'dbsize');
+  }
+
+  // TTL
+  function ttl(key: string, callback?: Callback): MethodReturn {
+    return request(callback, 'ttl', key);
   }
 
   // BITCOUNT
@@ -117,224 +238,30 @@ export default function client(url?: string, token?: string) {
     return request(callback, 'bitpos', key, bit);
   }
 
-  // DBSIZE
-  function dbsize(callback?: Callback): MethodReturn {
-    return request(callback, 'dbsize');
-  }
-
-  // DECR
-  function decr(key: string, callback?: Callback): MethodReturn {
-    return request(callback, 'decr', key);
-  }
-
-  // DECRBY
-  function decrby(
-    key: string,
-    decrement: number,
-    callback?: Callback
-  ): MethodReturn {
-    return request(callback, 'decrby', key, decrement);
-  }
-
-  // DEL
-  function del(
-    key: string,
-    fields: string[],
-    callback?: Callback
-  ): MethodReturn {
-    return request(callback, 'del', key, ...fields);
-  }
-
-  // TODO:EVAL
-  // TODO:EVALSHA
-
-  // EXISTS
-  function exists(key: string | string[], callback?: Callback): MethodReturn {
-    if (Array.isArray(key)) {
-      return request(callback, 'exists', ...key);
-    }
-    return request(callback, 'exists', key);
-  }
-
-  // EXPIRE
-  function expire(
-    key: string,
-    seconds: number,
-    option?: 'NX' | 'XX' | 'GT' | 'LT',
-    callback?: Callback
-  ): MethodReturn {
-    if (option) {
-      return request(callback, 'expire', key, seconds, option);
-    }
-    return request(callback, 'expire', key, seconds);
-  }
-
-  // FLUSHALL
-  function flushall(mode?: 'ASYNC', callback?: Callback): MethodReturn {
-    if (mode) {
-      return request(callback, 'flushall', mode);
-    }
-    return request(callback, 'flushall');
-  }
-
-  // FLUSHDB
-  function flushdb(mode?: 'ASYNC', callback?: Callback): MethodReturn {
-    if (mode) {
-      return request(callback, 'flushdb', mode);
-    }
-    return request(callback, 'flushdb');
-  }
-
-  // GET
-  function get(key: string, callback?: Callback): MethodReturn {
-    return request(callback, 'get', key);
-  }
-
-  // TODO:GETBIT
-
-  // GETRANGE
-  function getrange(
-    key: string,
-    start: number,
-    end: number,
-    callback?: Callback
-  ): MethodReturn {
-    return request(callback, 'getrange', key, start, end);
-  }
-
-  // TODO:GETSET
-  // TODO:HDEL
-  // TODO:HEXISTS
-  // TODO:HGET
-  // TODO:HGETALL
-  // TODO:HINCRBY
-  // TODO:HINCRBYFLOAT
-  // TODO:HKEYS
-  // TODO:HLEN
-  // TODO:HMGET
-  // TODO:HSCAN
-  // TODO:HSET
-  // TODO:HSETNX
-  // TODO:HVALS
-  // TODO:INCR
-  // TODO:INCRBY
-  // TODO:INCRBYFLOAT
-
-  // KEYS
-  function keys(pattern: string, callback?: Callback): MethodReturn {
-    return request(callback, 'keys', pattern);
-  }
-
-  // TODO:LINDEX
-  // TODO:LINSERT
-  // TODO:LLEN
-  // TODO:LPOP
-  // TODO:LPUSH
-  // TODO:LPUSHX
-  // TODO:LRANGE
-  // TODO:LREM
-  // TODO:LSET
-  // TODO:LTRIM
-  // TODO:MGET
-
-  // MSET
-  function mset(values: string[], callback?: Callback): MethodReturn {
-    return request(callback, 'mset', ...values);
-  }
-
-  // TODO:MSETNX
-  // TODO:PEXPIRE
-  // TODO:PING
-  // TODO:PSETEX
-  // TODO:PTTL
-  // TODO:RANDOMKEY
-  // TODO:RENAME
-  // TODO:RENAMENX
-  // TODO:RPOP
-  // TODO:RPOPLPUSH
-  // TODO:RPUSH
-  // TODO:RPUSHX
-  // TODO:SADD
-  // TODO:SCAN
-  // TODO:SCARD
-  // TODO:SCRIPT EXISTS
-  // TODO:SCRIPT FLUSH
-  // TODO:SCRIPT LOAD
-  // TODO:SDIFF
-  // TODO:SDIFFSTORE
-
-  // SET
-  function set(key: string, value: string, callback?: Callback): MethodReturn {
-    return request(callback, 'set', key, value);
-  }
-
-  // TODO:SETBIT
-  // TODO:SETEX
-  // TODO:SETNX
-  // TODO:SETRANGE
-  // TODO:SINTER
-  // TODO:SINTERSTORE
-  // TODO:SISMEMBER
-  // TODO:SMEMBERS
-  // TODO:SMOVE
-  // TODO:SPOP
-  // TODO:SRANDMEMBER
-  // TODO:SREM
-  // TODO:SSCAN
-  // TODO:STRLEN
-  // TODO:SUNION
-  // TODO:SUNIONSTORE
-  // TODO:TOUCH
-
-  // TTL
-  function ttl(key: string, callback?: Callback): MethodReturn {
-    return request(callback, 'ttl', key);
-  }
-
-  // TODO:TYPE
-  // TODO:ZADD
-  // TODO:ZCARD
-  // TODO:ZCOUNT
-  // TODO:ZINCRBY
-  // TODO:ZINTERSTORE
-  // TODO:ZLEXCOUNT
-  // TODO:ZPOPMAX
-  // TODO:ZPOPMIN
-  // TODO:ZRANGE
-  // TODO:ZRANGEBYLEX
-  // TODO:ZRANGEBYSCORE
-  // TODO:ZRANK
-  // TODO:ZREM
-  // TODO:ZREMRANGEBYLEX
-  // TODO:ZREMRANGEBYRANK
-  // TODO:ZREMRANGEBYSCORE
-  // TODO:ZREVRANGE
-  // TODO:ZREVRANGEBYLEX
-  // TODO:ZREVRANGEBYSCORE
-  // TODO:ZREVRANK
-  // TODO:ZSCAN
-  // TODO:ZSCORE
-  // TODO:ZUNIONSTORE
-
   return {
     auth,
+
+    // String
     append,
+    decr,
+    decrby,
+    get,
+    getrange,
+    getset,
+    mset,
+    set,
+
+    //
     bitcount,
     bitop,
     bitpos,
     dbsize,
-    decr,
-    decrby,
     del,
     exists,
     expire,
     flushall,
     flushdb,
-    get,
-    getrange,
     keys,
-    mset,
-    set,
     ttl,
   };
 }
---
+++ test/getset.test.ts
@@ -0,0 +1,17 @@
+import { get, getset, set } from '../src';
+
+describe('get command', () => {
+  const key = 'myKey';
+  const value1 = 'hi';
+  const value2 = 'hey';
+
+  it('basic', async () => {
+    await set(key, value1);
+
+    const { data: data1 } = await getset(key, value2);
+    expect(data1).toEqual(value1);
+
+    const { data: data2 } = await get(key);
+    expect(data2).toEqual(value2);
+  });
+});

57d73762bc31294b651abccc6910b6fb7bae01fb
enesakar <enesakar@gmail.com>
2021-10-21 11:36:15 -0700

    example

---
+++ examples/workers-with-upstash/index.js
@@ -0,0 +1,28 @@
+import { auth, incr } from 'upstash-redis';
+
+auth(UPSTASH_REDIS_REST_URL, UPSTASH_REDIS_REST_TOKEN);
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  const url = new URL(request.url);
+
+  if (url.pathname !== '/') {
+    return new Response();
+  }
+
+  const { data: count } = await incr('workers-count');
+
+  return new Response(html(count), {
+    headers: {
+      'content-type': 'text/html;charset=UTF-8',
+    },
+  });
+}
+
+const html = (count) => `
+  <h1>Cloudflare Workers with Upstash Redis</h1>
+  <h2>Count: ${count}</h2>
+`;

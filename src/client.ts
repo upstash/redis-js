6d7610f5afb010704d0aff1e9a312a771b2f4446
ademilter <ademilter@gmail.com>
2021-11-01 21:53:13 +0300

    fix: readfromedge

--- src/client.ts
+++ src/client.ts
@@ -53,7 +53,7 @@ function Upstash(): Upstash {
     const arg0 = arguments[0];
     const arg1 = arguments[1];
 
-    OPTIONS = { url: '', token: '', edgeUrl: '', readFromEdge: false };
+    OPTIONS = { url: '', token: '', edgeUrl: '', readFromEdge: true };
 
     // Upstash({})
     if (isObject(arg0)) {
@@ -61,7 +61,11 @@ function Upstash(): Upstash {
       OPTIONS.url = url;
       OPTIONS.token = token;
       OPTIONS.edgeUrl = edgeUrl;
-      OPTIONS.readFromEdge = readFromEdge;
+      if (readFromEdge === true || readFromEdge === false) {
+        OPTIONS.readFromEdge = edgeUrl ?? readFromEdge;
+      } else {
+        OPTIONS.readFromEdge = !!edgeUrl;
+      }
     }
     // Upstash(url, token)
     else if (isString(arg0) && isString(arg1)) {
@@ -138,9 +142,12 @@ function Upstash(): Upstash {
       );
     }
 
+    console.log('--', isRequestDefaultEdge, isRequestCustomEdge);
+
     if (isRequestDefaultEdge || isRequestCustomEdge) {
       const command = encodeURI(parts.join('/'));
       const edgeUrlWithPath = `${OPTIONS.edgeUrl}/${command}`;
+      console.log(edgeUrlWithPath);
       promise = fetchData(edgeUrlWithPath, {
         method: 'GET',
       });

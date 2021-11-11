48a3f7215dd9789197020e3954bd1f9c5845ff44
Adem ilter <ademilter@gmail.com>
2021-11-11 13:34:58 +0300

    fix export

--- examples/nodejs/index.js
+++ examples/nodejs/index.js
@@ -3,12 +3,16 @@ import upstash from '@upstash/redis';
 
 dotenv.config();
 
-const { echo } = upstash.default(
-  process.env.UPSTASH_REDIS_REST_URL,
-  process.env.UPSTASH_REDIS_REST_TOKEN
-);
+const redis = upstash.default({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  edgeUrl: process.env.UPSTASH_REDIS_EDGE_URL,
+});
 
 (async function run() {
-  const res = await echo();
-  console.log(res);
+  const res1 = await redis.set('node', '23');
+  console.log(res1);
+
+  const res2 = await redis.get('node');
+  console.log(res2);
 })();

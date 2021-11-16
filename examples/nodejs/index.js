18d432ce576f5e2fc5fc6d334946bf7492cecad8
Adem ilter <ademilter@gmail.com>
2021-11-16 21:48:57 +0300

    fix import default (#13)

--- examples/nodejs/index.js
+++ examples/nodejs/index.js
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv';
-import upstash from '@upstash/redis';
+import upstash, { auth, get } from '@upstash/redis';
 
 dotenv.config();
 
-const redis = upstash.default({
+const redis = upstash({
   url: process.env.UPSTASH_REDIS_REST_URL,
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
   edgeUrl: process.env.UPSTASH_REDIS_EDGE_URL,
@@ -13,6 +13,12 @@ const redis = upstash.default({
   const res1 = await redis.set('node', '23');
   console.log(res1);
 
-  const res2 = await redis.get('node');
+  auth({
+    url: process.env.UPSTASH_REDIS_REST_URL,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN,
+    edgeUrl: process.env.UPSTASH_REDIS_EDGE_URL,
+  });
+
+  const res2 = await get('node');
   console.log(res2);
 })();

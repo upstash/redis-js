4e6e2aba583f02d9e9f33d9a2333344823cd1b96
Andreas Thomas <dev@chronark.com>
2022-03-12 10:42:34 +0100

    fix: parse numbers larger than MAX_SAFE_INTEGER as string

--- pkg/command.ts
+++ pkg/command.ts
@@ -8,7 +8,7 @@ import { parseResponse } from "./util"
  * TData represents what the user will enter or receive,
  * TResult is the raw data returned from upstash, which may need to be transformed or parsed.
  */
-export abstract class Command<TData, TResult> {
+export class Command<TData, TResult> {
   public readonly command: string[]
   public deserialize: (result: TResult) => TData
   /**

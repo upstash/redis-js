02e1e689943473a5d48b7780572231a1f2597ad2
Andreas Thomas <dev@chronark.com>
2022-03-22 22:16:18 +0100

    fix: change return type of `exists` to number

--- pkg/commands/exists.ts
+++ pkg/commands/exists.ts
@@ -4,7 +4,7 @@ import { Command } from "./command"
 /**
  * @see https://redis.io/commands/exists
  */
-export class ExistsCommand extends Command<0 | 1, "0" | "1"> {
+export class ExistsCommand extends Command<number, number> {
   constructor(...keys: NonEmptyArray<string>) {
     super(["exists", ...keys])
   }

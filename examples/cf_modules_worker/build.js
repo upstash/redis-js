4a63d9808444c86fb341b376f846e4566007ba43
Andreas Thomas <dev@chronark.com>
2022-04-14 20:20:55 +0200

    feat: allow passing USER=andreas __CFBundleIdentifier=com.microsoft.VSCode COMMAND_MODE=unix2003 LOGNAME=andreas PATH=/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/go/bin:/Library/Apple/usr/bin:/Users/andreas/.cargo/bin SSH_AUTH_SOCK=/private/tmp/com.apple.launchd.uHupVV5LkM/Listeners SHELL=/bin/zsh HOME=/Users/andreas __CF_USER_TEXT_ENCODING=0x1F5:0x0:0x0 TMPDIR=/var/folders/bp/ssqmchhn3797vnwq9j3t81dh0000gn/T/ XPC_SERVICE_NAME=0 XPC_FLAGS=0x0 ORIGINAL_XDG_CURRENT_DESKTOP=undefined TERM_PROGRAM=vscode TERM_PROGRAM_VERSION=1.66.2 LANG=en_US.UTF-8 COLORTERM=truecolor VSCODE_GIT_IPC_HANDLE=/var/folders/bp/ssqmchhn3797vnwq9j3t81dh0000gn/T/vscode-git-fcdaa034c3.sock VSCODE_GIT_ASKPASS_NODE=/Applications/Visual Studio Code.app/Contents/MacOS/Electron VSCODE_GIT_ASKPASS_EXTRA_ARGS=--ms-enable-electron-run-as-node VSCODE_GIT_ASKPASS_MAIN=/Applications/Visual Studio Code.app/Contents/Resources/app/extensions/git/dist/askpass-main.js GIT_ASKPASS=/Applications/Visual Studio Code.app/Contents/Resources/app/extensions/git/dist/askpass.sh PWD=/Users/andreas/github/upstash/upstash-redis TERM=xterm-256color SHLVL=1 OLDPWD=/Users/andreas/github/upstash/upstash-redis/examples/cf_modules_worker HOMEBREW_PREFIX=/opt/homebrew HOMEBREW_CELLAR=/opt/homebrew/Cellar HOMEBREW_REPOSITORY=/opt/homebrew MANPATH=/opt/homebrew/share/man:: INFOPATH=/opt/homebrew/share/info: ZSH=/Users/andreas/.oh-my-zsh PAGER=less LESS=-R LSCOLORS=Gxfxcxdxbxegedabagacad _=/usr/bin/env in cloudflare modules workers

---
+++ examples/cf_modules_worker/build.js
@@ -0,0 +1,21 @@
+import path from "path"
+import { fileURLToPath } from "url"
+import { build } from "esbuild"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+try {
+  await build({
+    bundle: true,
+    sourcemap: true,
+    format: "esm",
+    target: "esnext",
+    entryPoints: [path.join(__dirname, "src", "index.ts")],
+    outdir: path.join(__dirname, "dist"),
+    outExtension: { ".js": ".mjs" },
+  })
+} catch (err) {
+  console.error(err)
+  process.exitCode = 1
+}

c064f63aa11cbc5f08dc2abba58749473fb879a9
Andreas Thomas <dev@chronark.com>
2022-04-20 20:02:48 +0200

    feat: add eval and script commands

---
+++ pkg/commands/evalsha.ts
@@ -0,0 +1,10 @@
+import { Command } from "./command"
+
+/**
+ * @see https://redis.io/commands/evalsha
+ */
+export class EvalshaCommand<TArgs extends unknown[], TData> extends Command<unknown, TData> {
+  constructor(sha: string, keys: string[], args?: TArgs) {
+    super(["evalsha", sha, keys.length, ...keys, ...(args ?? [])])
+  }
+}

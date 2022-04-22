2979ffbe23d30a927b04ff8f2faadd1f3735232a
Andreas Thomas <dev@chronark.com>
2022-04-22 15:52:53 +0200

    fix: only load node modules when actually on node

--- pkg/nodejs.ts
+++ pkg/nodejs.ts
@@ -4,28 +4,28 @@ import https from "https";
 import http from "http";
 import "isomorphic-fetch";
 
-export type { UpstashRequest, Requester, UpstashResponse };
+export type { Requester, UpstashRequest, UpstashResponse };
 
 /**
  * Connection credentials for upstash redis.
  * Get them from https://console.upstash.com/redis/<uuid>
  */
 export type RedisConfigNodejs = {
-	/**
+  /**
    * UPSTASH_REDIS_REST_URL
    */
-	url: string,
-	/**
+  url: string;
+  /**
    * UPSTASH_REDIS_REST_TOKEN
    */
-	token: string,
+  token: string;
 };
 
 /**
  * Serverless redis client for upstash.
  */
 export class Redis extends core.Redis {
-	/**
+  /**
    * Create a new redis client by providing the url and token
    *
    * @example
@@ -36,9 +36,9 @@ export class Redis extends core.Redis {
    * });
    * ```
    */
-	constructor(config: RedisConfigNodejs);
+  constructor(config: RedisConfigNodejs);
 
-	/**
+  /**
    * Create a new redis client by providing a custom `Requester` implementation
    *
    * @example
@@ -55,38 +55,42 @@ export class Redis extends core.Redis {
    * const redis = new Redis(requester)
    * ```
    */
-	constructor(requesters: Requester);
-	constructor(configOrRequester: RedisConfigNodejs | Requester) {
-		if ("request" in configOrRequester) {
-			super(configOrRequester);
-			return;
-		}
+  constructor(requesters: Requester);
+  constructor(configOrRequester: RedisConfigNodejs | Requester) {
+    if ("request" in configOrRequester) {
+      super(configOrRequester);
+      return;
+    }
 
-		let agent: http.Agent | https.Agent | undefined = undefined;
+    let agent: http.Agent | https.Agent | undefined = undefined;
 
-		if (typeof window === "undefined") {
-			const protocol = new URL(configOrRequester.url).protocol;
-			switch (protocol) {
-				case "https:":
-					agent = new https.Agent({ keepAlive: true });
+    if (
+      typeof window === "undefined" &&
+      typeof process !== "undefined" &&
+      process.release?.name === "node"
+    ) {
+      const protocol = new URL(configOrRequester.url).protocol;
+      switch (protocol) {
+        case "https:":
+          agent = new https.Agent({ keepAlive: true });
 
-					break;
-				case "http:":
-					agent = new https.Agent({ keepAlive: true });
-					break;
-			}
-		}
+          break;
+        case "http:":
+          agent = new https.Agent({ keepAlive: true });
+          break;
+      }
+    }
 
-		const client = new HttpClient({
-			baseUrl: configOrRequester.url,
-			headers: { authorization: `Bearer ${configOrRequester.token}` },
-			options: { agent },
-		});
+    const client = new HttpClient({
+      baseUrl: configOrRequester.url,
+      headers: { authorization: `Bearer ${configOrRequester.token}` },
+      options: { agent },
+    });
 
-		super(client);
-	}
+    super(client);
+  }
 
-	/**
+  /**
    * Create a new Upstash Redis instance from environment variables.
    *
    * Use this to automatically load connection secrets from your environment
@@ -95,24 +99,24 @@ export class Redis extends core.Redis {
    * This tries to load `UPSTASH_REDIS_REST_URL` and `UPSTASH_REDIS_REST_TOKEN` from
    * your environment using `process.env`.
    */
-	static fromEnv(): Redis {
-		if (typeof process?.env === "undefined") {
-			throw new Error(
-				"Unable to get environment variables, `process.env` is undefined. If you are deploying to cloudflare, please use `Redis.onCloudflare()` instead",
-			);
-		}
-		const url = process.env["UPSTASH_REDIS_REST_URL"];
-		if (!url) {
-			throw new Error(
-				"Unable to find environment variable: `UPSTASH_REDIS_REST_URL`",
-			);
-		}
-		const token = process.env["UPSTASH_REDIS_REST_TOKEN"];
-		if (!token) {
-			throw new Error(
-				"Unable to find environment variable: `UPSTASH_REDIS_REST_TOKEN`",
-			);
-		}
-		return new Redis({ url, token });
-	}
+  static fromEnv(): Redis {
+    if (typeof process?.env === "undefined") {
+      throw new Error(
+        "Unable to get environment variables, `process.env` is undefined. If you are deploying to cloudflare, please use `Redis.onCloudflare()` instead",
+      );
+    }
+    const url = process.env["UPSTASH_REDIS_REST_URL"];
+    if (!url) {
+      throw new Error(
+        "Unable to find environment variable: `UPSTASH_REDIS_REST_URL`",
+      );
+    }
+    const token = process.env["UPSTASH_REDIS_REST_TOKEN"];
+    if (!token) {
+      throw new Error(
+        "Unable to find environment variable: `UPSTASH_REDIS_REST_TOKEN`",
+      );
+    }
+    return new Redis({ url, token });
+  }
 }

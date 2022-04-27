4165430d3dc6eb4acfe2848543ee1ddda5e37e6a
Andreas Thomas <dev@chronark.com>
2022-04-27 12:33:08 +0200

    ci: debug

--- examples/cloudflare-service-worker/src/index.ts
+++ examples/cloudflare-service-worker/src/index.ts
@@ -1,26 +1,26 @@
-import { Redis } from "@upstash/redis/cloudflare";
+import { Redis } from '@upstash/redis/cloudflare'
 
-const redis = Redis.fromEnv();
+const redis = Redis.fromEnv()
 
-addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
-});
+addEventListener('fetch', (event) => {
+  event.respondWith(handleRequest(event.request))
+})
 
 async function handleRequest(request: Request) {
-  const url = new URL(request.url);
+  const url = new URL(request.url)
 
-  if (url.pathname !== "/") {
-    return new Response();
+  if (url.pathname !== '/') {
+    return new Response()
   }
 
-  const count = await redis.incr("cloudflare-workers-count");
+  const count = await redis.incr('cloudflare-workers-count')
 
   return new Response(html(count), {
-    headers: { "content-type": "text/html;charset=UTF-8" },
-  });
+    headers: { 'content-type': 'text/html;charset=UTF-8' },
+  })
 }
 
-const html = (count) => `
+const html = (count: number) => `
   <h1>Cloudflare Workers with Upstash Redis</h1>
   <h2>Count: ${count}</h2>
-`;
+`

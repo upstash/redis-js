86f40dd7d90fbec39d455282096b8f108b7fc0a0
Andreas Thomas <dev@chronark.com>
2022-04-27 16:38:11 +0200

    chore: rename example

---
+++ examples/cloudflare-modules-worker/src/index.ts
@@ -0,0 +1,16 @@
+import { Redis } from "@upstash/redis/cloudflare";
+import type { Bindings } from "bindings";
+
+export default {
+  async fetch(request: Request, env: Bindings) {
+    const url = new URL(request.url);
+    console.log({ url });
+    const redis = Redis.fromEnv(env);
+
+    const count = await redis.incr("cloudflare-modules-worker-count");
+
+    return new Response(
+      `<h1>Cloudflare Workers with Upstash Redis</h1><h2>Count: ${count}</h2>`,
+    );
+  },
+};

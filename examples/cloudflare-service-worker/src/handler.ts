11c6c9266e279680a023fc1e5454824c696ca00e
Andreas Thomas <dev@chronark.com>
2022-05-23 16:45:11 +0200

    ci: trigger

--- examples/cloudflare-service-worker/src/handler.ts
+++
@@ -1,7 +0,0 @@
-import { Redis } from "@upstash/redis/cloudflare";
-
-export async function handleRequest(_request: Request): Promise<Response> {
-  const redis = Redis.fromEnv();
-  const count = await redis.incr("cloudflare_service_worker_counter");
-  return new Response(JSON.stringify({ count }));
-}

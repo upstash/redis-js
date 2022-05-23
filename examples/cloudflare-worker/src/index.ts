b083c6dc76d760b8a918293b20c1b312e7d63aef
Andreas Thomas <dev@chronark.com>
2022-05-23 16:46:29 +0200

    ci: trigger

--- examples/cloudflare-worker/src/index.ts
+++ examples/cloudflare-worker/src/index.ts
@@ -7,7 +7,7 @@ export default {
     console.log({ url });
     const redis = Redis.fromEnv(env);
 
-    const count = await redis.incr("cloudflare-modules-worker-count");
+    const count = await redis.incr("cloudflare-worker-count");
 
     return new Response(
       `<h1>Cloudflare Workers with Upstash Redis</h1><h2>Count: ${count}</h2>`,

33b4de0da523912a8261d591481c3c955e0de874
Andreas Thomas <dev@chronark.com>
2022-05-23 16:28:20 +0200

    ci: trigger

---
+++ examples/nextjs_edge/README.md
@@ -0,0 +1,17 @@
+# Next.js + Tailwind CSS Example
+
+This example shows how to use [Tailwind CSS](https://tailwindcss.com/)
+[(v3.0)](https://tailwindcss.com/blog/tailwindcss-v3) with Next.js. It follows
+the steps outlined in the official
+[Tailwind docs](https://tailwindcss.com/docs/guides/nextjs).
+
+## How to use
+
+Execute
+[`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app)
+with [npm](https://docs.npmjs.com/cli/init) or
+[Yarn](https://yarnpkg.com/lang/en/docs/cli/create/) to bootstrap the example:
+
+```bash
+yarn create next-app -e https://github.com/upstash/next-template your-app-name
+```
---
+++ examples/nextjs_edge/pages/api/_middleware.ts
@@ -0,0 +1,28 @@
+import type { NextFetchEvent, NextRequest } from "next/server";
+import { Redis } from "@upstash/redis";
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+});
+export default async function middleware(
+  request: NextRequest,
+  event: NextFetchEvent,
+): Promise<Response | undefined> {
+  const start = Date.now();
+
+  /**
+   * We're prefixing the key for our automated tests.
+   * This is to avoid collisions with other tests.
+   */
+  const key = ["vercel", process.env.VERCEL_GIT_COMMIT_SHA, "nextjs_middleware"]
+    .join("_");
+
+  const counter = await redis.incr(key);
+  console.log("Middleware", counter);
+  return new Response(
+    JSON.stringify({ counter, latency: Date.now() - start }),
+    {
+      status: 200,
+    },
+  );
+}

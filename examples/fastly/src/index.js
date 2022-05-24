3007a49b6f0a4e660965ca9558e929e8ec5def76
Andreas Thomas <dev@chronark.com>
2022-05-24 11:04:55 +0200

    cf deploy (#96)
    * fix: vercel edge runs again
    
    * docs
    
    * v1.4.0 (#91)
    
    * feat(http): remove automatic fetch polyfill (#88)
    
    * feat(http): remove automatic fetch polyfill
    
    * fix: add underscore to unused example variables
    
    * test: expect correct value
    
    * ci: remove release dry-run flag (#89)
    
    * feat: add import path for fetch polyfilled redis (#90)
    
    * feat: add import path for fetch polyfilled redis
    
    * docs: update nodejs section
    
    * ci: deploy to cloudflare
    
    * test: use deno to test fastly
    
    * ci: deploy to fastly
    
    * ci: deploy to fastly
    
    * ci: use publish
    
    * ci: trigger
    
    * ci: trigger
    
    * ci: trigger
    
    * ci: trigger
    
    * ci: trigger
    
    * ci: trigger
    
    * ci: trigger

--- examples/fastly/src/index.js
+++ examples/fastly/src/index.js
@@ -3,12 +3,17 @@ import { Redis } from "@upstash/redis/fastly";
 addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
 
 async function handleRequest(_event) {
-  const redis = new Redis({
-    url: "<UPSTASH_REDIS_REST_URL>",
-    token: "<UPSTASH_REDIS_REST_TOKEN>",
-    backend: "upstash-db", // same name you used in `fastly.toml`
-  });
-
-  const counter = await redis.incr("fastly");
-  return new Response(`Counter: ${counter}`);
+  try {
+    const redis = new Redis({
+      url: "<UPSTASH_REDIS_REST_URL>",
+      token: "<UPSTASH_REDIS_REST_TOKEN>",
+      backend: "upstash-db", // same name you used in `fastly.toml`
+    });
+    const count = await redis.incr("fastly");
+    return new Response(JSON.stringify({ count }), {
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (err) {
+    return new Response(err.message, { status: 500 });
+  }
 }

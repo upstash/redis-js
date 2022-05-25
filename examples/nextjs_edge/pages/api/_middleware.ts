a8a043a0f325a35da6cc2425304992f5983a42c0
Andreas Thomas <dev@chronark.com>
2022-05-25 17:48:58 +0200

    test: use Redis.fromEnv()

--- examples/nextjs_edge/pages/api/_middleware.ts
+++ examples/nextjs_edge/pages/api/_middleware.ts
@@ -1,21 +1,24 @@
 import type { NextFetchEvent, NextRequest } from "next/server";
 import { Redis } from "@upstash/redis";
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL!,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
-});
+// const redis = new Redis({
+//   url: process.env.UPSTASH_REDIS_REST_URL!,
+//   token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+// });
 export default async function middleware(
   _request: NextRequest,
   _event: NextFetchEvent,
 ): Promise<Response | undefined> {
   const start = Date.now();
-
+  const redis = Redis.fromEnv();
   /**
    * We're prefixing the key for our automated tests.
    * This is to avoid collisions with other tests.
    */
-  const key = ["vercel", process.env.VERCEL_GIT_COMMIT_SHA, "nextjs_middleware"]
-    .join("_");
+  const key = [
+    "vercel",
+    process.env.VERCEL_GIT_COMMIT_SHA,
+    "nextjs_middleware",
+  ].join("_");
 
   const counter = await redis.incr(key);
   console.log("Middleware", counter);

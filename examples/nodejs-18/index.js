2d0b8753c4e5e447c837618012fe27e761ce5a66
Andreas Thomas <dev@chronark.com>
2022-05-30 09:54:14 +0200

    ci: add cloudflare account id

---
+++ examples/nodejs-18/index.js
@@ -0,0 +1,19 @@
+import { Redis } from "@upstash/redis";
+
+const redis = Redis.fromEnv();
+async function run() {
+  const key = "key";
+  const value = { hello: "world" };
+
+  const res1 = await redis.set(key, value);
+  console.log(res1);
+
+  const res2 = await redis.get(key);
+  console.log(typeof res2, res2);
+
+  if (JSON.stringify(value) != JSON.stringify(res2)) {
+    throw new Error("value not equal");
+  }
+}
+
+run();

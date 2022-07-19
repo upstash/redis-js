3a257f8af5a23baac400ee86089e7e681d570ade
Andreas Thomas <dev@chronark.com>
2022-07-19 17:16:53 +0300

    style: fmt

--- examples/nextjs_edge/test.ts
+++ examples/nextjs_edge/test.ts
@@ -5,9 +5,15 @@ if (!deploymentURL) {
   throw new Error("DEPLOYMENT_URL not set");
 }
 
-Deno.test("works", async () => {
+Deno.test("works", async (t) => {
   console.log({ deploymentURL });
   const url = `${deploymentURL}/api/counter`;
+  setTimeout(()=>{
+    console.error("manual timeout")
+    Deno.exit(1)
+  }, 5000)
+
+
   const res = await fetch(url);
   assertEquals(res.status, 200);
   const counterString = res.headers.get("Counter");

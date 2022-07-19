0999bfc429b4ec9f3585450fe8d84e056bde11e5
Andreas Thomas <dev@chronark.com>
2022-07-19 13:08:55 +0300

    style: fmt

--- examples/nextjs_edge/pages/api/counter.ts
+++ examples/nextjs_edge/pages/api/counter.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default (_req: NextApiRequest, res: NextApiResponse) => {
   res.status(200);
 };

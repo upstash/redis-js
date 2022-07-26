83859c0afa5a62270aaef6e61d9e10980f7077f4
Andreas Thomas <dev@chronark.com>
2022-07-26 09:43:18 +0200

    fix: move type out of inline

--- pkg/commands/zadd.ts
+++ pkg/commands/zadd.ts
@@ -10,6 +10,15 @@ export type ZAddCommandOptions =
 
 export type ZAddCommandOptionsWithIncr = ZAddCommandOptions & { incr: true };
 
+/**
+ * This type is defiend up here because otherwise it would be automatically formatted into
+ * multiple lines by Deno. As a result of that, Deno will add a comma to the end and then
+ * complain about the comma being there...
+ */
+type Arg2<TData> =
+  | ScoreMember<TData>
+  | ZAddCommandOptions
+  | ZAddCommandOptionsWithIncr;
 export type ScoreMember<TData> = { score: number; member: TData };
 /**
  * @see https://redis.io/commands/zadd
@@ -35,13 +44,7 @@ export class ZAddCommand<TData = string> extends Command<
     opts?: CommandOptions<number | null, number | null>,
   );
   constructor(
-    [key, arg1, ...arg2]: [
-      string,
-      | ScoreMember<TData>
-      | ZAddCommandOptions
-      | ZAddCommandOptionsWithIncr,
-      ...ScoreMember<TData>[],
-    ],
+    [key, arg1, ...arg2]: [string, Arg2<TData>, ...ScoreMember<TData>[]],
     opts?: CommandOptions<number | null, number | null>,
   ) {
     const command: unknown[] = ["zadd", key];

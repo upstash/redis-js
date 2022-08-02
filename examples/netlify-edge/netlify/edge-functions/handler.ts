b97b57520a55c77df17924a72d76c2744c0388a4
Andreas Thomas <dev@chronark.com>
2022-08-02 09:47:03 +0200

    feat: netlify-edge example

---
+++ examples/netlify-edge/netlify/edge-functions/handler.ts
@@ -0,0 +1,12 @@
+import { Context } from "netlify:edge";
+import { Redis } from "https://deno.land/x/upstash_redis@v1.10.2/mod.ts";
+
+const redis = Redis.fromEnv();
+export default async (_req: Request, ctx: Context) => {
+  ctx.log("Hello");
+
+  return new Response(JSON.stringify({
+    message: "Hello World",
+    counter: await redis.incr("netlify"),
+  }));
+};

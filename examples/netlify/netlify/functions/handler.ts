c22e2fe135feb2be2604e624b4909961959bcc6b
Andreas Thomas <dev@chronark.com>
2022-08-02 12:23:17 +0200

    feat: example for netlify functions (#135)
    * feat: example for netlify functions
    
    * fix: lint error
    
    * feat: netlify-edge example
    
    * ci: install dependencies
    
    * fix: sleep command
    
    * ci: pin dependencies of nextjs
    
    * test: fix assertion
    
    * test: fix url
    
    * test: fix url
    
    * style: fmt
    
    * style: fmt
    
    * ci. add env
    
    * ci. add env
    
    * ci: set port
    
    * ci: set port
    
    * ci: set port
    
    * ci: set port
    
    * ci: set port
    
    * ci: set port
    
    * ci: set port
    
    * ci: trigger
    
    * ci: trigger
    
    * ci: trigger
    
    * ci: trigger
    
    * ci: trigger
    
    * ci: trigger
    
    * ci: trigger
    
    * ci: trigger
    
    * ci: trigger

---
+++ examples/netlify/netlify/functions/handler.ts
@@ -0,0 +1,16 @@
+import { Handler } from "@netlify/functions";
+import { Redis } from "@upstash/redis/with-fetch";
+
+const redis = Redis.fromEnv();
+
+const handler: Handler = async (_event, _context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Hello World",
+      counter: await redis.incr("netlify"),
+    }),
+  };
+};
+
+export { handler };
---
+++ examples/netlify/test.ts
@@ -0,0 +1,17 @@
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+
+const deploymentURL = Deno.env.get("DEPLOYMENT_URL");
+if (!deploymentURL) {
+  throw new Error("DEPLOYMENT_URL not set");
+}
+
+Deno.test("works", async () => {
+  console.log({ deploymentURL });
+  const url = `${deploymentURL}/.netlify/functions/handler`;
+  console.log({ url });
+
+  const res = await fetch(url);
+  assertEquals(res.status, 200);
+  const json = (await res.json()) as { counter: number };
+  assertEquals(typeof json.counter, "number");
+});

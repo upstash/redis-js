cb3203baaa996e56e6beaf9b4b6db8a1acd5b7ee
Andreas Thomas <dev@chronark.com>
2022-08-23 11:27:40 +0200

    fix: add trycatch

--- examples/netlify-edge/netlify/edge-functions/handler.ts
+++ examples/netlify-edge/netlify/edge-functions/handler.ts
@@ -4,9 +4,13 @@ import { Redis } from "https://deno.land/x/upstash_redis@v1.12.0-rc.1/mod.ts";
 const redis = Redis.fromEnv();
 export default async (_req: Request, ctx: Context) => {
   ctx.log("Hello");
-
-  return new Response(JSON.stringify({
-    message: "Hello World",
-    counter: await redis.incr("netlify-edge"),
-  }));
+  try {
+    return new Response(JSON.stringify({
+      message: "Hello World",
+      counter: await redis.incr("netlify-edge"),
+    }));
+  } catch (err) {
+    ctx.log(err);
+    return new Response(err.message, { status: 500 });
+  }
 };

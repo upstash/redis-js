db77ec7c7ab4045f0e98f589de14ac785fe9c601
Andreas Thomas <dev@chronark.com>
2022-12-19 13:39:41 +0100

    fix: smembers generic type (#283)
    * fix: smembers generic type
    
    * test: fix types

--- pkg/commands/smembers.ts
+++ pkg/commands/smembers.ts
@@ -3,11 +3,12 @@ import { Command, CommandOptions } from "./command.ts";
 /**
  * @see https://redis.io/commands/smembers
  */
-export class SMembersCommand<TData = string> extends Command<
-  unknown[],
-  TData[]
-> {
-  constructor(cmd: [key: string], opts?: CommandOptions<unknown[], TData[]>) {
+export class SMembersCommand<TData extends unknown[] = string[]>
+  extends Command<
+    unknown[],
+    TData
+  > {
+  constructor(cmd: [key: string], opts?: CommandOptions<unknown[], TData>) {
     super(["smembers", ...cmd], opts);
   }
 }

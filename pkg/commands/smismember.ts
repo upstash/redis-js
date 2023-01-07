34d8591fc4ad5c9d1898dee8242bd04006c62fdf
Andreas Thomas <dev@chronark.com>
2023-01-07 20:54:28 +0100

    fix: smismember

--- pkg/commands/smismember.ts
+++ pkg/commands/smismember.ts
@@ -7,9 +7,9 @@ export class SMIsMemberCommand<TMembers extends unknown[]> extends Command<
   (0 | 1)[]
 > {
   constructor(
-    cmd: [key: string, ...members: TMembers],
+    cmd: [key: string, members: TMembers],
     opts?: CommandOptions<("0" | "1")[], (0 | 1)[]>,
   ) {
-    super(["smismember", ...cmd], opts);
+    super(["smismember", cmd[0], ...cmd[1]], opts);
   }
 }

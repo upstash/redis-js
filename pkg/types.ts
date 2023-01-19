7f00da2708ccb4fee9865f1819468a88261c745f
Andreas Thomas <dev@chronark.com>
2023-01-19 10:47:43 +0100

    feat: allow setting telemetry from outside

--- pkg/types.ts
+++ pkg/types.ts
@@ -1,2 +1,22 @@
 export type CommandArgs<TCommand extends new (...args: any) => any> =
   ConstructorParameters<TCommand>[0];
+
+export type Telemetry = {
+  /**
+   * Upstash-Telemetry-Sdk
+   * @example @upstash/redis@v1.1.1
+   */
+  sdk?: string;
+
+  /**
+   * Upstash-Telemetry-Platform
+   * @example cloudflare
+   */
+  platform?: string;
+
+  /**
+   * Upstash-Telemetry-Runtime
+   * @example node@v18
+   */
+  runtime?: string;
+};

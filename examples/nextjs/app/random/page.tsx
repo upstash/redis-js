e6c48394c9916990e18c9e88965924b7c2597453
chronark <dev@chronark.com>
2023-04-14 15:36:52 +0200

    test: add app dir test

---
+++ examples/nextjs/app/random/page.tsx
@@ -0,0 +1,16 @@
+import { Redis } from "@upstash/redis";
+const redis = Redis.fromEnv();
+export default async function Page() {
+  const size = await redis.scard("random");
+  if (size === 0) {
+    await redis.sadd("random", "Hello", "World", "Welcome", "to", "Upstash");
+  }
+
+  const random = await redis.srandmember<string>("random");
+
+  return (
+    <div>
+      {random}
+    </div>
+  );
+}

4ed10016b0974dbf4fd746ff68c0d03dd35ad116
Andreas Thomas <dev@chronark.com>
2023-04-14 15:53:12 +0200

    ci (#341)
    * test: add argument to unlink
    
    * ci: wip
    
    * ci: fix env variables
    
    * ci: install node
    
    * ci: install npm
    
    * ci: skip
    
    * test: add app dir test
    
    * test: upgrade checkout to v3

--- examples/netlify-edge/netlify/edge-functions/handler.ts
+++ examples/netlify-edge/netlify/edge-functions/handler.ts
@@ -1,16 +1,16 @@
-import { Context } from "netlify:edge";
-import { Redis } from "https://deno.land/x/upstash_redis@v1.19.3/mod.ts";
+import type { Context } from "https://edge.netlify.com";
+import { Redis } from "https://deno.land/x/upstash_redis/mod.ts";
 
 const redis = Redis.fromEnv();
-export default async (_req: Request, ctx: Context) => {
-  ctx.log("Hello");
+export default async (_req: Request, _ctx: Context) => {
+  console.log("Hello");
   try {
     return new Response(JSON.stringify({
       message: "Hello World",
       counter: await redis.incr("netlify-edge"),
     }));
   } catch (err) {
-    ctx.log(err);
+    console.error(err);
     return new Response(err.message, { status: 500 });
   }
 };

d6be2b2a82cb5efc19e8a9056e33a99d031fc556
chronark <dev@chronark.com>
2023-10-19 14:59:52 +0200

    ci: wip

--- examples/deno/main.ts
+++
@@ -1,9 +0,0 @@
-import { serve } from "https://deno.land/std@0.177.0/http/server";
-import { Redis } from "https://deno.land/x/upstash_redis/mod";
-
-serve(async (_req: Request) => {
-  const redis = Redis.fromEnv();
-  const counter = await redis.incr("deno deploy counter");
-
-  return new Response(JSON.stringify({ counter }), { status: 200 });
-});

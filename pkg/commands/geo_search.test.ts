14a41e06fe5ced2e82ec56c93f0bc8db7b59f193
ogzhanolguncu <ogzhan11@gmail.com>
2023-10-20 17:29:24 +0300

    Add GEOSEARCH command to sdk

---
+++ pkg/commands/geo_search.test.ts
@@ -0,0 +1,172 @@
+import { keygen, newHttpClient } from "../test-utils.ts";
+
+import { assertEquals } from "https://deno.land/std@0.177.0/testing/asserts.ts";
+
+import { afterAll } from "https://deno.land/std@0.177.0/testing/bdd.ts";
+import { GeoAddCommand } from "./geo_add.ts";
+import { GeoSearchCommand } from "./geo_search.ts";
+
+const client = newHttpClient();
+const { newKey, cleanup } = keygen();
+afterAll(cleanup);
+
+Deno.test("should return distance successfully in meters", async () => {
+  const key = newKey();
+  await new GeoAddCommand([
+    key,
+    { longitude: 13.361389, latitude: 38.115556, member: "Palermo" },
+    { longitude: 15.087269, latitude: 37.502669, member: "Catania" },
+  ]).exec(client);
+
+  const res = await new GeoSearchCommand([
+    key,
+    { type: "FROMLONLAT", coordinate: { lon: 15, lat: 37 } },
+    { type: "BYRADIUS", radius: 200, radiusType: "KM" },
+    "ASC",
+  ]).exec(client);
+
+  assertEquals(res, [{ member: "Catania" }, { member: "Palermo" }]);
+});
+
+Deno.test("should return members within the specified box", async () => {
+  const key = newKey();
+
+  await new GeoAddCommand([
+    key,
+    { longitude: 13.361389, latitude: 38.115556, member: "Palermo" },
+    { longitude: 15.087269, latitude: 37.502669, member: "Catania" },
+  ]).exec(client);
+
+  const res = await new GeoSearchCommand([
+    key,
+    { type: "FROMLONLAT", coordinate: { lon: 14, lat: 37.5 } },
+    { type: "BYBOX", rect: { width: 200, height: 200 }, rectType: "KM" },
+    "ASC",
+  ]).exec(client);
+
+  assertEquals(res, [{ member: "Palermo" }, { member: "Catania" }]);
+});
+
+Deno.test("should return members with coordinates, distances, and hashes", async () => {
+  const key = newKey();
+
+  await new GeoAddCommand([
+    key,
+    { longitude: 13.361389, latitude: 38.115556, member: "Palermo" },
+    { longitude: 15.087269, latitude: 37.502669, member: "Catania" },
+  ]).exec(client);
+
+  const res = await new GeoSearchCommand([
+    key,
+    { type: "FROMLONLAT", coordinate: { lon: 14, lat: 37.5 } },
+    { type: "BYRADIUS", radius: 200, radiusType: "KM" },
+    "ASC",
+    { withHash: true, withCoord: true, withDist: true },
+  ]).exec(client);
+
+  assertEquals(res, [
+    {
+      member: "Palermo",
+      coord: { long: 13.361389338970184, lat: 38.1155563954963 },
+      dist: 88.526,
+      hash: "3479099956230698",
+    },
+    {
+      member: "Catania",
+      coord: { long: 15.087267458438873, lat: 37.50266842333162 },
+      dist: 95.9406,
+      hash: "3479447370796909",
+    },
+  ]);
+});
+
+Deno.test("should return members with distances, and hashes", async () => {
+  const key = newKey();
+
+  await new GeoAddCommand([
+    key,
+    { longitude: 13.361389, latitude: 38.115556, member: "Palermo" },
+    { longitude: 15.087269, latitude: 37.502669, member: "Catania" },
+  ]).exec(client);
+
+  const res = await new GeoSearchCommand([
+    key,
+    { type: "FROMLONLAT", coordinate: { lon: 14, lat: 37.5 } },
+    { type: "BYRADIUS", radius: 200, radiusType: "KM" },
+    "ASC",
+    { withHash: true, withDist: true },
+  ]).exec(client);
+
+  assertEquals(res, [
+    {
+      member: "Palermo",
+      dist: 88.526,
+      hash: "3479099956230698",
+    },
+    {
+      member: "Catania",
+      dist: 95.9406,
+      hash: "3479447370796909",
+    },
+  ]);
+});
+
+Deno.test("should return members with and coordinates", async () => {
+  const key = newKey();
+
+  await new GeoAddCommand([
+    key,
+    { longitude: 13.361389, latitude: 38.115556, member: "Palermo" },
+    { longitude: 15.087269, latitude: 37.502669, member: "Catania" },
+  ]).exec(client);
+
+  const res = await new GeoSearchCommand([
+    key,
+    { type: "FROMLONLAT", coordinate: { lon: 14, lat: 37.5 } },
+    { type: "BYRADIUS", radius: 200, radiusType: "KM" },
+    "ASC",
+    { withCoord: true },
+  ]).exec(client);
+
+  assertEquals(res, [
+    {
+      member: "Palermo",
+      coord: { long: 13.361389338970184, lat: 38.1155563954963 },
+    },
+    {
+      member: "Catania",
+      coord: { long: 15.087267458438873, lat: 37.50266842333162 },
+    },
+  ]);
+});
+
+Deno.test("should return members with coordinates, and hashes", async () => {
+  const key = newKey();
+
+  await new GeoAddCommand([
+    key,
+    { longitude: 13.361389, latitude: 38.115556, member: "Palermo" },
+    { longitude: 15.087269, latitude: 37.502669, member: "Catania" },
+  ]).exec(client);
+
+  const res = await new GeoSearchCommand([
+    key,
+    { type: "FROMLONLAT", coordinate: { lon: 14, lat: 37.5 } },
+    { type: "BYRADIUS", radius: 200, radiusType: "KM" },
+    "ASC",
+    { withHash: true, withCoord: true },
+  ]).exec(client);
+
+  assertEquals(res, [
+    {
+      member: "Palermo",
+      hash: "3479099956230698",
+      coord: { long: 13.361389338970184, lat: 38.1155563954963 },
+    },
+    {
+      member: "Catania",
+      hash: "3479447370796909",
+      coord: { long: 15.087267458438873, lat: 37.50266842333162 },
+    },
+  ]);
+});

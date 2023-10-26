01cfa5eeefc595b7bcd72a1e314353bff3eb5046
ogzhanolguncu <ogzhan11@gmail.com>
2023-10-26 15:10:49 +0300

    Update test file

--- pkg/commands/geo_hash.test.ts
+++ pkg/commands/geo_hash.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.203.0/assert/assert_equals.ts";
+import { describe, expect, test } from "bun:test";
 import { newHttpClient } from "../test-utils.ts";
 
 import { GeoAddCommand } from "./geo_add.ts";
@@ -6,29 +6,31 @@ import { GeoHashCommand } from "./geo_hash.ts";
 
 const client = newHttpClient();
 
-Deno.test("should accept two member array and return valid hash", async () => {
-  const key = "Sicily";
-  const members = ["Palermo", "Catania"];
-  await new GeoAddCommand([
-    key,
-    { longitude: 13.361389, latitude: 38.115556, member: members[0] },
-    { longitude: 15.087269, latitude: 37.502669, member: members[1] },
-  ]).exec(client);
+describe("GEOHASH tests", () => {
+  test("should accept two member array and return valid hash", async () => {
+    const key = "Sicily";
+    const members = ["Palermo", "Catania"];
+    await new GeoAddCommand([
+      key,
+      { longitude: 13.361389, latitude: 38.115556, member: members[0] },
+      { longitude: 15.087269, latitude: 37.502669, member: members[1] },
+    ]).exec(client);
 
-  const response = await new GeoHashCommand([key, members]).exec(client);
-  assertEquals(response.length, 2);
-});
+    const response = await new GeoHashCommand([key, members]).exec(client);
+    expect(response.length).toEqual(2);
+  });
 
-Deno.test("should accept three different string members and return valid hash", async () => {
-  const key = "Sicily";
-  const members = ["Palermo", "Catania", "Marsala"];
-  await new GeoAddCommand([
-    key,
-    { longitude: 13.361389, latitude: 38.115556, member: members[0] },
-    { longitude: 15.087269, latitude: 37.502669, member: members[1] },
-    { longitude: 12.4372, latitude: 37.7981, member: members[2] },
-  ]).exec(client);
+  test("should accept three different string members and return valid hash", async () => {
+    const key = "Sicily";
+    const members = ["Palermo", "Catania", "Marsala"];
+    await new GeoAddCommand([
+      key,
+      { longitude: 13.361389, latitude: 38.115556, member: members[0] },
+      { longitude: 15.087269, latitude: 37.502669, member: members[1] },
+      { longitude: 12.4372, latitude: 37.7981, member: members[2] },
+    ]).exec(client);
 
-  const response = await new GeoHashCommand([key, "Palermo", "Catania", "Marsala"]).exec(client);
-  assertEquals(response.length, 3);
+    const response = await new GeoHashCommand([key, "Palermo", "Catania", "Marsala"]).exec(client);
+    expect(response.length).toEqual(3);
+  });
 });

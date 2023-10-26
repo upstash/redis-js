592d82ad57260f66893ba112ec5cfaa61655e58b
ogzhanolguncu <ogzhan11@gmail.com>
2023-10-26 13:46:43 +0300

    Disable sourcemaps and allow minify

--- tsup.config.js
+++ tsup.config.js
@@ -4,8 +4,10 @@ export default defineConfig({
   entry: ["platforms/nodejs.ts", "platforms/cloudflare.ts", "platforms/fastly.ts"],
   format: ["cjs", "esm"],
   splitting: false,
-  sourcemap: true,
+  sourcemap: false,
   clean: true,
   bundle: true,
   dts: true,
+  minify: true,
+  minifyWhitespace: true,
 });

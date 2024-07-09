963abaea76f1827446801368b461bf8c87089901
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-07-09 17:53:15 +0300

    add: lua script test

--- pkg/read-your-writes.test.ts
+++ pkg/read-your-writes.test.ts
@@ -9,56 +9,78 @@ import { Redis } from "./redis";
 const { cleanup } = keygen();
 afterAll(cleanup);
 describe("Read Your Writes", () => {
-  test("successfully retrieves Upstash-Sync-Token in the response header and updates local state", async () => {
-    const client = newHttpClient();
-    const initialSync = client._upstashSyncToken;
-    await new SetCommand(["key", "value"]).exec(client);
-    const updatedSync = client._upstashSyncToken;
-    await new SetCommand(["key", "value"]).exec(client);
+	test("successfully retrieves Upstash-Sync-Token in the response header and updates local state", async () => {
+		const client = newHttpClient();
+		const initialSync = client._upstashSyncToken;
+		await new SetCommand(["key", "value"]).exec(client);
+		const updatedSync = client._upstashSyncToken;
+		await new SetCommand(["key", "value"]).exec(client);
 
-    expect(updatedSync).not.toEqual(initialSync);
-  });
+		expect(updatedSync).not.toEqual(initialSync);
+	});
 
-  test.skip("succesfully updates sync state with pipeline", async () => {
-    const client = newHttpClient();
+	test.skip("succesfully updates sync state with pipeline", async () => {
+		const client = newHttpClient();
 
-    await new SetCommand(["key", "value"]).exec(client);
-    const initialSync1 = client._upstashSyncToken;
-    console.log("initialSync1", initialSync1);
+		await new SetCommand(["key", "value"]).exec(client);
+		const initialSync1 = client._upstashSyncToken;
+		console.log("initialSync1", initialSync1);
 
-    const initialSync = client._upstashSyncToken;
-    console.log("initialSync", initialSync);
-    const { pipeline } = new Redis(client);
-    const p = pipeline();
+		const initialSync = client._upstashSyncToken;
+		console.log("initialSync", initialSync);
+		const { pipeline } = new Redis(client);
+		const p = pipeline();
 
-    p.set("key1", "value1");
-    p.set("key2", "value2");
-    p.set("key3", "value3");
-    p.set("key4", "value4");
-    p.set("key5", "value5");
+		p.set("key1", "value1");
+		p.set("key2", "value2");
+		p.set("key3", "value3");
+		p.set("key4", "value4");
+		p.set("key5", "value5");
 
-    await p.exec();
+		await p.exec();
 
-    const updatedSync = client._upstashSyncToken;
+		const updatedSync = client._upstashSyncToken;
 
-    await new SetCommand(["key", "value"]).exec(client);
-    const updatedSync2 = client._upstashSyncToken;
+		await new SetCommand(["key", "value"]).exec(client);
+		const updatedSync2 = client._upstashSyncToken;
 
-    console.log("updatedSync", updatedSync);
-    console.log("updatedSync2", updatedSync2);
-  });
+		console.log("updatedSync", updatedSync);
+		console.log("updatedSync2", updatedSync2);
+	});
 
-  test("updates after each element of promise.all", async () => {
-    const client = newHttpClient();
-    let currentSync = client._upstashSyncToken;
+	test("updates after each element of promise.all", async () => {
+		const client = newHttpClient();
+		let currentSync = client._upstashSyncToken;
 
-    const promises = Array.from({ length: 3 }, (_, i) =>
-      new SetCommand([`key${i}`, `value${i}`]).exec(client).then(() => {
-        expect(client._upstashSyncToken).not.toEqual(currentSync);
-        currentSync = client._upstashSyncToken;
-      }),
-    );
+		const promises = Array.from({ length: 3 }, (_, i) =>
+			new SetCommand([`key${i}`, `value${i}`]).exec(client).then(() => {
+				expect(client._upstashSyncToken).not.toEqual(currentSync);
+				currentSync = client._upstashSyncToken;
+			}),
+		);
 
-    await Promise.all(promises);
-  });
+		await Promise.all(promises);
+	});
+
+	test("updates after successful lua script call", async () => {
+
+		const s = `redis.call('SET', 'mykey', 'myvalue')
+		return 1
+		`
+
+		const client = newHttpClient();
+		await new SetCommand(["key", "value"]).exec(client);
+		const initialSync = client._upstashSyncToken;
+
+		const redis = new Redis(client);
+		const script = redis.createScript(s);
+
+		await script.exec([], []);
+
+		const updatedSync = client._upstashSyncToken;
+
+		expect(updatedSync).not.toEqual(initialSync);
+
+
+	})
 });

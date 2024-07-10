cf10255948c53f185d089da029edee373507ff0a
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-07-10 09:25:55 +0300

    format tests

--- pkg/read-your-writes.test.ts
+++ pkg/read-your-writes.test.ts
@@ -1,16 +1,16 @@
 import { keygen, newHttpClient } from "./test-utils";
 
 import { afterAll, describe, expect, test } from "bun:test";
-// import { GetCommand } from "./get";
-// import { SetCommand } from "./set";
+
 import { SetCommand } from "./commands/set";
 import { Redis } from "./redis";
 
+const client = newHttpClient();
 const { cleanup } = keygen();
 afterAll(cleanup);
-describe("Read Your Writes", () => {
+describe("Read Your Writes Feature", () => {
 	test("successfully retrieves Upstash-Sync-Token in the response header and updates local state", async () => {
-		const client = newHttpClient();
+
 		const initialSync = client._upstashSyncToken;
 		await new SetCommand(["key", "value"]).exec(client);
 		const updatedSync = client._upstashSyncToken;
@@ -19,37 +19,26 @@ describe("Read Your Writes", () => {
 		expect(updatedSync).not.toEqual(initialSync);
 	});
 
-	test.skip("succesfully updates sync state with pipeline", async () => {
-		const client = newHttpClient();
-
-		await new SetCommand(["key", "value"]).exec(client);
-		const initialSync1 = client._upstashSyncToken;
-		console.log("initialSync1", initialSync1);
-
+	test("succesfully updates sync state with pipeline", async () => {
 		const initialSync = client._upstashSyncToken;
-		console.log("initialSync", initialSync);
+
 		const { pipeline } = new Redis(client);
 		const p = pipeline();
 
 		p.set("key1", "value1");
 		p.set("key2", "value2");
 		p.set("key3", "value3");
-		p.set("key4", "value4");
-		p.set("key5", "value5");
 
 		await p.exec();
 
 		const updatedSync = client._upstashSyncToken;
 
-		await new SetCommand(["key", "value"]).exec(client);
-		const updatedSync2 = client._upstashSyncToken;
 
-		console.log("updatedSync", updatedSync);
-		console.log("updatedSync2", updatedSync2);
+		expect(initialSync).not.toEqual(updatedSync);
 	});
 
 	test("updates after each element of promise.all", async () => {
-		const client = newHttpClient();
+
 		let currentSync = client._upstashSyncToken;
 
 		const promises = Array.from({ length: 3 }, (_, i) =>
@@ -68,8 +57,6 @@ describe("Read Your Writes", () => {
 		return 1
 		`
 
-		const client = newHttpClient();
-		await new SetCommand(["key", "value"]).exec(client);
 		const initialSync = client._upstashSyncToken;
 
 		const redis = new Redis(client);
@@ -80,7 +67,6 @@ describe("Read Your Writes", () => {
 		const updatedSync = client._upstashSyncToken;
 
 		expect(updatedSync).not.toEqual(initialSync);
-
 
 	})
 });

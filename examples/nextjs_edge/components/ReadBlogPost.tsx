03b7aa3ed2cbd40b6463a22d6215e73f7249823f
yunusemreozdemir <emreozdemir@mail.com>
2024-07-18 10:48:58 +0300

    fix: remove old vercel functions related examples

--- examples/nextjs_edge/components/ReadBlogPost.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ReadBlogPost({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div className="p-4 text-center bg-gray-100">{children}</div>;
-}

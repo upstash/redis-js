80f67fb5197f9949e61da33e980d457334ee2826
ytkimirti <yusuftaha9@gmail.com>
2024-09-23 10:41:56 +0300

    chore: move to new build pipeline

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["platforms/nodejs.ts", "platforms/cloudflare.ts", "platforms/fastly.ts"],
   format: ["cjs", "esm"],
-  splitting: true,
-  sourcemap: false,
   clean: true,
-  bundle: true,
   dts: true,
-  minify: true,
-  minifyWhitespace: true,
 });

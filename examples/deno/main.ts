779e0c05e9daa082db9499f669e7f292180da552
CahidArda <cahidardaooz@hotmail.com>
2024-11-04 11:23:44 +0300

    fix: bump deno

--- examples/deno/main.ts
+++ examples/deno/main.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
 import { Redis } from "https://esm.sh/@upstash/redis@latest";
 
 serve(async (_req: Request) => {

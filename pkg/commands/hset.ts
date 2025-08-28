74dcb418a4da66330a9170649df7425553bcb8d2
thecrowkeep <69373707+thecrowkeep@users.noreply.github.com>
2025-08-28 14:10:14 -0500

    fix type constraint
    passing an object with strongly typed keys is not assignable to Record<string, unknown>. this change ensures that objects pass the type constraint even when strongly typed.

--- pkg/commands/hset.ts
+++ pkg/commands/hset.ts
@@ -4,9 +4,9 @@ import { Command } from "./command";
 /**
  * @see https://redis.io/commands/hset
  */
-export class HSetCommand<TData> extends Command<number, number> {
+export class HSetCommand<TData extends object> extends Command<number, number> {
   constructor(
-    [key, kv]: [key: string, kv: Record<string, TData>],
+    [key, kv]: [key: string, kv: TData],
     opts?: CommandOptions<number, number>
   ) {
     super(["hset", key, ...Object.entries(kv).flatMap(([field, value]) => [field, value])], opts);

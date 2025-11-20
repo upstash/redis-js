0f8090fa31c83e6b610c4f1bf3016fe88cbc8847
Martin VON SCHLEDORN <Martin.vonschledorn@renewa.de>
2025-11-20 11:51:58 +0100

    Fix: zremrangebyscore should accept string | number for min/max scores
    The zremrangebyscore command had incorrect TypeScript type definitions
    that only accepted number for min and max parameters, but the Redis
    protocol and Upstash REST API accept string values like '-inf' and '+inf'.
    
    This change aligns zremrangebyscore with the existing zcount command,
    which already uses number | string for score range parameters.
    
    Changes:
    - Updated min parameter type from 'number' to 'number | string'
    - Updated max parameter type from 'number' to 'number | string'
    
    Fixes #1395

--- pkg/commands/zremrangebyscore.ts
+++ pkg/commands/zremrangebyscore.ts
@@ -4,7 +4,7 @@ import { Command } from "./command";
  * @see https://redis.io/commands/zremrangebyscore
  */
 export class ZRemRangeByScoreCommand extends Command<number, number> {
-  constructor(cmd: [key: string, min: number, max: number], opts?: CommandOptions<number, number>) {
+  constructor(cmd: [key: string, min: number | string, max: number | string], opts?: CommandOptions<number, number>) {
     super(["zremrangebyscore", ...cmd], opts);
   }
 }

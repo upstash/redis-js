b635b96229a7171553081bdc9e188b14a9deb94a
Martin <134405310+mvonschledorn@users.noreply.github.com>
2025-11-26 15:49:31 +0100

    Fix: zremrangebyscore should accept string | number for min/max scores (#1396)
    * Fix: zremrangebyscore should accept string | number for min/max scores
    
    The zremrangebyscore command had incorrect TypeScript type definitions
    that only accepted number for min and max parameters, but the Redis
    protocol and Upstash REST API accept string values like '-inf' and '+inf'.
    
    This change aligns zremrangebyscore with the existing zcount command,
    which already uses number | string for score range parameters.
    
    Changes:
    - Updated min parameter type from 'number' to 'number | string'
    - Updated max parameter type from 'number' to 'number | string'
    
    Fixes #1395
    
    * fix: narrow down allowed strings in zremrangebyscore
    
    * fix: add more tests
    
    ---------
    
    Co-authored-by: CahidArda <cahidardaooz@hotmail.com>

--- pkg/commands/zremrangebyscore.test.ts
+++ pkg/commands/zremrangebyscore.test.ts
@@ -1,15 +1,17 @@
 import { keygen, newHttpClient, randomID } from "../test-utils";
 
-import { afterAll, expect, test } from "bun:test";
+import { afterEach, beforeEach, expect, test } from "bun:test";
 import { ZAddCommand } from "./zadd";
 import { ZRemRangeByScoreCommand } from "./zremrangebyscore";
 
 const client = newHttpClient();
 
 const { newKey, cleanup } = keygen();
-afterAll(cleanup);
-test("returns the number of removed elements", async () => {
-  const key = newKey();
+
+const key = newKey();
+
+afterEach(cleanup);
+beforeEach(async () => {
   const member1 = randomID();
   const member2 = randomID();
   const member3 = randomID();
@@ -19,6 +21,19 @@ test("returns the number of removed elements", async () => {
     { score: 2, member: member2 },
     { score: 3, member: member3 },
   ]).exec(client);
+});
+
+test("returns the number of removed elements", async () => {
   const res = await new ZRemRangeByScoreCommand([key, 1, 2]).exec(client);
   expect(res).toEqual(2);
+});
+
+test("returns the number of removed elements with inf", async () => {
+  const res = await new ZRemRangeByScoreCommand([key, "-inf", "+inf"]).exec(client);
+  expect(res).toEqual(3);
+});
+
+test("returns the number of removed elements with exclusive range", async () => {
+  const res = await new ZRemRangeByScoreCommand([key, "(1", "(3"]).exec(client);
+  expect(res).toEqual(1);
 });

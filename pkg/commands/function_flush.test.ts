12a70d036663339a7706cefcff2efef00fecbb1f
ytkimirti <yusuftaha9@gmail.com>
2025-12-22 14:26:35 +0300

    test: fix

--- pkg/commands/function_flush.test.ts
+++ pkg/commands/function_flush.test.ts
@@ -4,17 +4,7 @@ import { FunctionFlushCommand } from "./function_flush";
 
 describe("FUNCTION FLUSH", () => {
   test("builds correct command (no mode)", () => {
-    const c = new FunctionFlushCommand([]);
+    const c = new FunctionFlushCommand();
     expect(c.command).toEqual(["function", "flush"]);
-  });
-
-  test("builds correct command (SYNC)", () => {
-    const c = new FunctionFlushCommand([{ mode: "SYNC" }]);
-    expect(c.command).toEqual(["function", "flush", "SYNC"]);
-  });
-
-  test("builds correct command (ASYNC)", () => {
-    const c = new FunctionFlushCommand([{ mode: "ASYNC" }]);
-    expect(c.command).toEqual(["function", "flush", "ASYNC"]);
   });
 });
